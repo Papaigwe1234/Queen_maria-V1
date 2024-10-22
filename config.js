0f7e87e4176d20f5d6308ae0330232d749629f1e
PAPA IGWE <papaigwe0@gmail.com>
2024-10-23 00:30:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const fs = require('fs')
 const { color } = require('./lib/myfunc')
 
 //owner
-global.owner = '2347043759577'
-global.nomerowner = ["2347043759577"]
+global.owner = '24160338758'
+global.nomerowner = ["24160338758"]
 
 // Apikey 
 
-global.skizo = 'David Cyril'
-global.casterix = 'DeeCeeXxx'
+global.skizo = 'Papa igwe'
+global.casterix = 'DADDY~IGWE'
 //watermark 
-global.packname = '*DAVID CYRIL*'
-global.author = 'QUEEN_ANITA-V1'
+global.packname = '*PAPA IGWE*'
+global.author = 'QUEEN_MARIA-V1'
 
 // cpanel 
 global.domain = 'https://' // deeceexxx
